a40234a3d2501fea76adf43393ced798e7d53a93
zust_hh <819473021@qq.com>
2018-11-27 22:39:14 +0800

    createLink end
    createLink end

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -7,8 +7,10 @@ const CREATE_LINK = gql`
   # 2
   mutation CreateLink($description: String!, $url: String!) {
     createLink(
-      description: $description,
-      url: $url,
+        data: {
+            description: $description,
+            url: $url,
+        }
     ) {
       url
       description
@@ -24,42 +26,44 @@ class CreateLink extends Component {
     }
     render() {
         return (
-            <Mutation mutation={CREATE_LINK}>
-                {createLink => (
-                    <div>
-                        <div className='flex flex-column mt3'>
-                            <input
-                                className='mb2'
-                                value={this.state.description}
-                                onChange={(e) => this.setState({
-                                    description: e.target.value
-                                })}
-                                type='text'
-                                placeholder='A description for the link'
-                            />
-                            <input
-                                className='mb2'
-                                value={this.state.url}
-                                onChange={(e) => this.setState({
-                                    url: e.target.value
-                                })}
-                                type='text'
-                                placeholder='The URL for the link'
-                            />
+            <Mutation mutation={CREATE_LINK} variables={{ description: this.state.description, url: this.state.url }}>
+                {createLink => {
+                    return (
+                        <div>
+                            <div className='flex flex-column mt3'>
+                                <input
+                                    className='mb2'
+                                    value={this.state.description}
+                                    onChange={(e) => this.setState({
+                                        description: e.target.value
+                                    })}
+                                    type='text'
+                                    placeholder='A description for the link'
+                                />
+                                <input
+                                    className='mb2'
+                                    value={this.state.url}
+                                    onChange={(e) => this.setState({
+                                        url: e.target.value
+                                    })}
+                                    type='text'
+                                    placeholder='The URL for the link'
+                                />
+                            </div>
+                            <button
+                                onClick={() => {
+                                    createLink();
+                                    this.setState({
+                                        description: '',
+                                        url: '',
+                                    })
+                                }}
+                            >
+                                Submit
+                            </button>
                         </div>
-                        <button
-                            onClick={() => {
-                                createLink({ variables: { description: this.state.description, url: this.state.url } });
-                                this.setState({
-                                    description: "",
-                                    url: ""
-                                })
-                            }}
-                        >
-                            Submit
-            </button>
-                    </div>
-                )}
+                    )
+                }}
             </Mutation>
         )
     }
