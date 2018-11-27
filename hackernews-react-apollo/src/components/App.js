d598e4e0dddedfe5d25f1deb9a280955e1c8fda8
zust_hh <819473021@qq.com>
2018-11-27 20:45:18 +0800

    add createlink
    add createlink

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -3,11 +3,16 @@ import logo from '../logo.svg';
 import '../styles/App.css';
 
 import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 class App extends Component {
   render() {
     return (
-      <LinkList />
+      <div>
+        <LinkList />
+        <CreateLink />
+      </div>
+      
     );
   }
 }
